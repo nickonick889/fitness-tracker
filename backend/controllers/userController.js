b49c7cbff0703bf63284be536e5108f5c329930c
Aloysius <aloysius.tan00@gmail.com>
2026-04-18 10:18:48 +0800

    UserController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+const User = require("../models/User");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const SALT_ROUNDS = 10;
+const SECRET = process.env.JWT_SECRET;
+const router = express.Router();
+
+const create =async(req, res) => {
+    const { username, password }= req.body;
+    try {
+        if (username === " ") {
+            return res.status(400).json({ err: "blank name" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+        const user = await User.create({ username, hashedPassword });
+
+        const payload = { user };
+        const token = jwt.sign(payload, SECRET);
+        res.status(201).json({ token });
+    } catch (err) {
+        res.status(500).json({ err });
+    }
+};
