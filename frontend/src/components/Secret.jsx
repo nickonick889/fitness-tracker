ee6b80561e0c56179bfa0483a0b347c54feaa275
Jiarui.Ng <josephngjiarui@gmail.com>
2026-04-29 19:42:53 +0800

    Commit merge

--- frontend/src/components/Secret.jsx
+++ frontend/src/components/Secret.jsx
@@ -1,22 +1,22 @@
-import { useContext, useState } from "react";
-import { UserContext } from "../contexts/UserContext";
-import { secret } from "../services/authService";
+// import { useContext, useState } from "react";
+// import { UserContext } from "../contexts/UserContext";
+// // import { secret } from "../services/authService";
 
-export default function Secret() {
-  const [message, setMessage] = useState("");
-  const { user } = useContext(UserContext);
+// export default function Secret() {
+//   const [message, setMessage] = useState("");
+//   const { user } = useContext(UserContext);
 
-  const handleClick = async () => {
-    const info = await secret();
-    setMessage(info.user._id);
-  };
+//   const handleClick = async () => {
+//     const info = await secret();
+//     setMessage(info.user._id);
+//   };
 
-  return (
-    <>
-      <h2>Secret</h2>
-      <p>{message}</p>
-      <p>{JSON.stringify(user)}</p>
-      <button onClick={handleClick}>Click</button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h2>Secret</h2>
+//       <p>{message}</p>
+//       <p>{JSON.stringify(user)}</p>
+//       <button onClick={handleClick}>Click</button>
+//     </>
+//   );
+// }
